a720a7ee2ece4fdaa6f881892bec4b84e8512c98
ytkt <yyttkktt@gmail.com>
2019-02-01 19:27:04 +0900

    Done ex4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,23 +62,42 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% vetorized y
+yv = zeros(m, num_labels);   % 5000 x 10
+for i=1:m
+  yv(i, y(i)) = 1;
+end
+
+a1 = [ones(m, 1) X];  % 5000 x 401
+z2 = a1 * Theta1';    % 5000 x 25
+
+a2 = [ones(m, 1) sigmoid(z2)]; % 5000 x 26
+z3 = a2 * Theta2';    % 5000 x 10
+
+a3 = sigmoid(z3);     % 5000 x 10
+
+J = sum(sum(-yv .* log(a3) - (1 - yv) .* log(1 - a3))) / m;
+
+% Regularize
+J += lambda / (2 * m) ...
+     * (sum(sum(Theta1(:, 2:end) .^ 2)) ...
+        + sum(sum(Theta2(:, 2:end) .^ 2)));
 
 
+% Backpropagation --------
+delta3 = a3 - yv;                                            % 5000 x 10
+delta2 = delta3 * Theta2(:, 2:end) .* sigmoidGradient(z2);   % 5000 x 25
+%      => [5000 x 10] * [10 x 25] .* [5000 x 25]
 
+Delta1 = delta2' * a1;
+Delta2 = delta3' * a2;
 
+Theta1_grad = Delta1 / m;
+Theta2_grad = Delta2 / m;
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Reguralize
+Theta1_grad(:, 2:end) += lambda / m * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) += lambda / m * Theta2(:, 2:end);
 
 % -------------------------------------------------------------
 
--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -12,18 +12,7 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
+g = arrayfun(@(x) x .* (1 - x), sigmoid(z));
 
 % =============================================================
 
