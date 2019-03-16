6fffa696899b27efa38747b6af32a1d587fb03cb
ytkt <yyttkktt@gmail.com>
2019-03-17 06:13:25 +0900

    Done ex8

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -12,28 +12,40 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
     %               end of the loop will compare the F1 score for this
     %               choice of epsilon and set it to be the best epsilon if
     %               it is better than the current choice of epsilon.
-    %               
+    %
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+  predictions = (pval < epsilon);
 
+  tp = sum((predictions == 1) & (yval == 1));
+  fp = sum((predictions == 1) & (yval == 0));
+  fn = sum((predictions == 0) & (yval == 1));
 
+  if tp + fp == 0
+    prec = 0;
+  else
+    prec = tp / (tp + fp);
+  end
 
+  if tp + fn == 0
+    rec = 0;
+  else
+    rec = tp / (tp + fn);
+  end
 
-
-
-
-
-
-
-
+  if prec + rec == 0
+    F1 = 0;
+  else
+    F1 = 2 * prec * rec / (prec + rec);
+  end
 
     % =============================================================
 
